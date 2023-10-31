b39779d504dca503d53d24a80aadcc6566de3cea
gkhanh <gkhanhle52@gmail.com>
2023-10-31 10:24:11 +0100

    Code clean up

--- imagePoseDetection.py
+++ imagePoseDetection.py
@@ -1,12 +1,12 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from cv2 import imshow
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
-import numpy as np
-import cv2
-from cv2 import imshow
-import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import csv
+
 
 def draw_landmarks_on_image(rgb_image, detection_result):
     pose_landmarks_list = detection_result.pose_landmarks
@@ -28,9 +28,10 @@ def draw_landmarks_on_image(rgb_image, detection_result):
             solutions.drawing_styles.get_default_pose_landmarks_style())
     return annotated_image
 
-#view original image
+
+# view original image
 img = cv2.imread("media/image.jpg")
-cv2.imshow('window',img)
+cv2.imshow('window', img)
 
 base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
 options = vision.PoseLandmarkerOptions(
@@ -43,12 +44,12 @@ image = mp.Image.create_from_file("./media/image.jpg")
 detection_result = detector.detect(image)
 
 annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-#view pose image
-imshow('window2',cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+# view pose image
+imshow('window2', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 
 segmentation_mask = detection_result.segmentation_masks[0].numpy_view()
 visualized_mask = np.repeat(segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
-#view segmentation mask
-imshow('window3',visualized_mask)
+# view segmentation mask
+imshow('window3', visualized_mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
