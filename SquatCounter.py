fad1e259a48963265cb06e4957bbf6bf327d505b
gkhanh <gkhanhle52@gmail.com>
2023-11-01 15:16:22 +0100

    complete Squat counter functions

--- SquatCounter.py
+++ SquatCounter.py
@@ -10,19 +10,19 @@ class RepCounter:
         self.currentKneeWindow = []
 
     def offerMeasurement(self, measurement):
-        (frameNumber, landmark, x, y, z) = measurement
+        [frameNumber, landmark, x, y, z] = measurement
         if landmark not in ["RIGHT_HIP", "RIGHT_KNEE"]:
             # Ignore any measurements not for the hip or knee
             return
 
         # At this point we only have data form the right hip and right knee
         if landmark == "RIGHT_HIP":
-            self.currentHipWindow.append(measurement.y)
+            self.currentHipWindow.append(y)
             if len(self.currentHipWindow) > 90:
                 self.currentHipWindow.pop(0)
 
         if landmark == "RIGHT_KNEE":
-            self.currentKneeWindow.append(measurement.y)
+            self.currentKneeWindow.append(y)
             if len(self.currentKneeWindow) > 90:
                 self.currentKneeWindow.pop(0)
 
@@ -46,6 +46,7 @@ class RepCounter:
                 if minValue == self.currentHipWindow[45]:
                     # Increment the repetition count
                     self.repetitions += 1
+                    print("Total Repetitions:", self.repetitions)
                     # Remove processed data to avoid counting the same repetition again
                     self.currentHipWindow = self.currentHipWindow[45:]
         return self.repetitions
