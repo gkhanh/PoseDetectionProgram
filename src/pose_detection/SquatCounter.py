f4ad92a818d8bcb02a79878d551171c4964f9924
gkhanh <gkhanhle52@gmail.com>
2023-11-07 11:03:30 +0100

    code cleanup

--- src/pose_detection/SquatCounter.py
+++ src/pose_detection/SquatCounter.py
@@ -1,8 +1,18 @@
+import mediapipe as mp
+from src.utils.plotDataFromCSV import CSVProcessor
+
+
 class SquatRepCounter:
     def __init__(self):
         self.repetitions = 0
         self.currentHipWindow = []
         self.currentKneeWindow = []
+
+        self.windowWidth = 60
+        self.rep = []
+        self.legitSquatTime = 30
+        self.lastSquatTime = -self.legitSquatTime  # Initialize to a value that allows the first squat to be counted
+
     def offerMeasurement(self, measurement):
         [_, landmark, _, y, _] = measurement
         measuringBodyPart = ["RIGHT_HIP", "RIGHT_KNEE"]
@@ -13,63 +23,25 @@ class SquatRepCounter:
         # At this point we only have data form the right hip and right knee
         if landmark == "RIGHT_HIP":
             self.currentHipWindow.append(y)
-            if len(self.currentHipWindow) > 90:
+            if len(self.currentHipWindow) > self.windowWidth:
                 self.currentHipWindow.pop(0)
 
         if landmark == "RIGHT_KNEE":
             self.currentKneeWindow.append(y)
-            if len(self.currentKneeWindow) > 90:
+            if len(self.currentKneeWindow) > self.windowWidth:
                 self.currentKneeWindow.pop(0)
 
-        if self.detectRepetition():
-            self.repetitions += 1
-            print("Total Repetitions:", self.repetitions)
+        self.detectRepetition()
 
     def detectRepetition(self):
-        if len(self.currentHipWindow) < 90:
-            # Ignore any startup
-            return False
-        avgKneeValue = round(sum(self.currentKneeWindow) / len(self.currentKneeWindow), 3)
-        avgHipValue = round(sum(self.currentHipWindow) / len(self.currentHipWindow), 3)
-        # Define PoseDetection threshold for squat detection
-        threshold = avgKneeValue - (avgKneeValue - avgHipValue)
-        currentHipValue = self.currentHipWindow[-1]
-        if currentHipValue < min(self.currentHipWindow[:-1]) and currentHipValue < threshold:
-            return True
-        return False
+        if len(self.currentHipWindow) < self.windowWidth:
+            # Not enough data yet
+            return
 
-    # def offerMeasurement(self, measurement):
-    #     [frameNumber, landmark, x, y, z] = measurement
-    #     if landmark not in ["RIGHT_HIP", "RIGHT_KNEE"]:
-    #         # Ignore any measurements not for the hip or knee
-    #         return
-    #     else:
-    #         if landmark == "RIGHT_HIP":
-    #             self.currentHipWindow.append(y)
-    #             if len(self.currentHipWindow) > 100:
-    #                 self.currentHipWindow.pop(0)
-    #
-    #         if landmark == "RIGHT_KNEE":
-    #             self.currentKneeWindow.append(y)
-    #             if len(self.currentKneeWindow) > 100:
-    #                 self.currentKneeWindow.pop(0)
-    #
-    #         if landmark == "RIGHT_HIP":
-    #             self.hipValue = y
-    #         if landmark == "RIGHT_KNEE":
-    #             self.kneeValue = y
-    #
-    #         self.detectRepetition()
-    #
-    # def detectRepetition(self):
-    #     # calculate threshold value
-    #     avg_diff = np.mean([abs(self.hipValue - self.kneeValue) for self.hipValue, self.kneeValue in
-    #                         zip(self.currentHipWindow, self.currentKneeWindow)])
-    #     if self.state == "stand":
-    #         if self.hipValue <= float(self.kneeValue) + avg_diff:
-    #             self.state = "squat"
-    #     elif self.state == "squat":
-    #         if self.hipValue > float(self.kneeValue) + avg_diff:
-    #             self.state = "stand"
-    #             self.repetitions += 1
-    #             print("Total Repetitions:", self.repetitions)
+        for i in range(self.windowWidth, -1, -1):
+            self.currentHipWindow = self.currentHipWindow[i:]
+            minHipWindow = min(self.currentHipWindow)
+            print("number:", minHipWindow)
+
+
+
