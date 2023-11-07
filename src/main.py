d95c0e549ecc7fc400f753c866bf8889f460632e
gkhanh <gkhanhle52@gmail.com>
2023-11-07 11:11:42 +0100

    code cleanup

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
-from src.utils.CSVWriter import CSVWriter
 from src.pose_detection.PoseModule import PoseDetector
-from src.pose_detection.SquatCounter import SquatRepCounter
+from src.utils.CSVWriter import CSVWriter
 
-repCounter = SquatRepCounter()
 csvWriter = CSVWriter(outputCSV='D:/MoveLabStudio/Assignment/PoseDetectionPrototype/output/output.csv')
 
 
 def myListener(landmark):
-    repCounter.offerMeasurement(landmark)
+    pass
 
 
 poseDetector = PoseDetector(
-    myListener
+    #myListener
 )
 
 poseDetector.runPoseCheckerWrapper(videoPath='D:/MoveLabStudio/Assignment/PoseDetectionPrototype/resources/video2.mp4')
