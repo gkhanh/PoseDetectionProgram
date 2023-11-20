96e23c7eefbfcc467b993b8346f1113791372b4d
gkhanh <gkhanhle52@gmail.com>
2023-11-20 16:07:56 +0100

    Update MathUtil class to calculate the angle

--- src/utils/MathUtils.py
+++ src/utils/MathUtils.py
@@ -3,16 +3,27 @@ import numpy as np
 
 
 # This class is for calculating angles and distances between landmark coordinates
-def calculate_angle(a, b, c):
-    a = np.array(a)  # First
-    b = np.array(b)  # Mid
-    c = np.array(c)  # End
 
-    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
-    angle = np.abs(radians * 180.0 / np.pi)
 
-    if angle > 180.0:
-        angle = 360.0 - angle
+class MathUtils:
+    def __init__(self) -> None:
+        pass
 
-    return angle
+    def calculateAngle(self, a, b, c):
+        a = np.array(a)  # First
+        b = np.array(b)  # Mid
+        c = np.array(c)  # End
+
+        radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+        angle = np.abs(radians * 180.0 / np.pi)
+
+        if angle > 180.0:
+            angle = 360.0 - angle
+
+        return angle
+
+    def calculateAngleWithXAxis(self, a, b):
+        x_diff = a[0] - b[0]
+        y_diff = a[1] - b[1]
+        return math.degrees(math.atan2(y_diff, x_diff))
 
