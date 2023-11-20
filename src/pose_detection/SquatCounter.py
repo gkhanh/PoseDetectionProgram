ecbf96cd9bca7af22f5c6c8da1cc7fdc4fbf3963
gkhanh <gkhanhle52@gmail.com>
2023-11-20 16:08:16 +0100

    code cleanup

--- src/pose_detection/SquatCounter.py
+++ src/pose_detection/SquatCounter.py
@@ -2,8 +2,11 @@ from typing import Optional
 
 from src.models.measurement import LandmarkPosition
 from src.pose_detection.PoseDetector import Measurement
-from src.utils.MathUtils import calculate_angle
+from src.utils.CalculatedAngles import CalculatedAngles
+from src.utils.MathUtils import MathUtils
 
+# Class for counting repetitions of squats
+# Currently this strategy is not used
 
 class SquatRepCounter:
     def __init__(self):
@@ -13,29 +16,23 @@ class SquatRepCounter:
         self.getMeasurementsInWindow = []
         self.kneeData = []
         self.hipData = []
-
         self.repetitions = []
 
     def offerMeasurement(self, measurement: Measurement):
         self.measurements.append(measurement)
-
         # Pop measurements when they are outside the windowSizeMillis
         while self.measurements[0].timestamp < measurement.timestamp - self.windowSizeMillis:
             self.measurements.pop(0)
-
         self.updateRepetitions()
 
     def updateRepetitions(self):
         # Filter only RIGHT_KNEE
         measurements = [measurement for measurement in self.measurements if
                         measurement.landmark == LandmarkPosition.RIGHT_HIP]
-
         for measurement in measurements:
             window = self.getWindowForMeasurement(measurement, measurements)
-
             if len(window) < 10:
                 continue
-
             # Returns a landmark that is recognized as a squat
             repetitionInWindow = self.findRepetition(window)
             if repetitionInWindow is not None:
@@ -43,15 +40,12 @@ class SquatRepCounter:
                 if self.isNotTooCloseToOtherRepetition(repetitionInWindow, self.repetitions):
                     self.repetitions.append(repetitionInWindow)
                     print("Repetition detected at " + str(repetitionInWindow.timestamp) + "ms")
-            self.curlCounterLogic()
 
     def getWindowForMeasurement(self, currentMeasurement, measurements):
         window = []
-
         # Look back in time to find the first measurement that is within the window
         startTimeOfWindow = currentMeasurement.timestamp - self.windowSizeMillis
         endTimeOfWindow = currentMeasurement.timestamp
-
         # Loop in reverse till we're outside the window
         for measurement in reversed(measurements):
             if startTimeOfWindow <= measurement.timestamp <= endTimeOfWindow:
@@ -59,15 +53,13 @@ class SquatRepCounter:
 
         return window
 
-    def findRepetition(self, window) -> Optional[Measurement]:
+    @staticmethod
+    def findRepetition(window) -> Optional[Measurement]:
         yCoordinates = [measurement.y for measurement in window]
-
         lowestPointIndex = yCoordinates.index(min(yCoordinates))
-
         # If we're going up or down it's not a local low
         if lowestPointIndex == 0 or lowestPointIndex == len(window) - 1:
             return None
-
         return window[lowestPointIndex]
 
     # function to check if the repetition is not too close to any other repetition
@@ -77,52 +69,3 @@ class SquatRepCounter:
             if abs(repetitionToConsider.timestamp - repetitions[i].timestamp) < self.windowSizeMillis:
                 return False
         return True
-
-    def curlCounterLogic(self):
-        # angle = calculate_angle(shoulder, elbow, wrist)
-        angleMin = []
-        angleMinHip = []
-        # rightHipList = [measurement.x, measurement.y for measurement in self.measurements if measurement.landmark == LandmarkPosition.RIGHT_HIP]
-        # rightKneeList = [measurement.x, measurement.y for measurement in self.measurements if measurement.landmark == LandmarkPosition.RIGHT_KNEE]
-        # rightShoulderList = [measurement.x, measurement.y for measurement in self.measurements if measurement.landmark == LandmarkPosition.RIGHT_SHOULDER]
-        # rightAnkleList = [measurement.x, measurement.y for measurement in self.measurements if measurement.landmark == LandmarkPosition.RIGHT_ANKLE]
-
-        rightHipList = [[measurement.x, measurement.y] for measurement in self.measurements if
-                        measurement.landmark == LandmarkPosition.RIGHT_HIP][0]
-        rightKneeList = [[measurement.x, measurement.y] for measurement in self.measurements if
-                         measurement.landmark == LandmarkPosition.RIGHT_KNEE][0]
-        rightShoulderList = [[measurement.x, measurement.y] for measurement in self.measurements if
-                             measurement.landmark == LandmarkPosition.RIGHT_SHOULDER][0]
-        rightAnkleList = [[measurement.x, measurement.y] for measurement in self.measurements if
-                          measurement.landmark == LandmarkPosition.RIGHT_ANKLE][0]
-
-        angleKnee = calculate_angle(rightHipList, rightKneeList, rightAnkleList)  # Knee joint angle
-        angleKnee = round(angleKnee, 2)
-
-        angleHip = calculate_angle(rightShoulderList, rightHipList, rightKneeList)
-        angleHip = round(angleHip, 2)
-
-        hipAngle = 180 - angleHip
-        kneeAngle = 180 - angleKnee
-
-        angleMin.append(angleKnee)
-        angleMinHip.append(angleHip)
-
-        # print(angleKnee)
-        if angleKnee > 150:
-            stage = "up"
-        if angleKnee <= 90: # and stage == 'up':
-            stage = "down"
-            counter = 0
-            counter += 1
-            print(counter)
-            min_ang = min(angleMin)
-            max_ang = max(angleMin)
-
-            min_ang_hip = min(angleMinHip)
-            max_ang_hip = max(angleMinHip)
-
-            # print(min(angleMin), " _ ", max(angleMin))
-            # print(min(angleMinHip), " _ ", max(angleMinHip))
-            angleMin = []
-            angleMinHip = []
