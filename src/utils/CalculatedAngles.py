760aebf020e286aed1312f81969314cd401db54f
gkhanh <gkhanhle52@gmail.com>
2023-11-22 16:34:00 +0100

    fixed error for printing wrong data

--- src/utils/CalculatedAngles.py
+++ src/utils/CalculatedAngles.py
@@ -13,35 +13,38 @@ class CalculatedAngles:
             # Find the RIGHT_KNEE
             rightKneeMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_KNEE:
                     rightKneeMeasurement = measurement
                     break
             if rightKneeMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_HIP
             rightHipMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_HIP:
                     rightHipMeasurement = measurement
                     break
             if rightHipMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_SHOULDER
             rightShoulderMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_SHOULDER:
                     rightShoulderMeasurement = measurement
                     break
             if rightShoulderMeasurement is None:
-                return 0
+                return None
 
             # Raise error if one of the measurements is missing
             if rightKneeMeasurement is None or rightHipMeasurement is None or rightShoulderMeasurement is None:
@@ -54,42 +57,45 @@ class CalculatedAngles:
             )
             return round(rightHipAngle, 2)
         except AttributeError:
-            return 0
+            return None
 
     def calculateRightKneeAngle(self):
         try:
             # Find the RIGHT_KNEE
             rightKneeMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_KNEE:
                     rightKneeMeasurement = measurement
                     break
             if rightKneeMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_ANKLE
             rightAnkleMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_ANKLE:
                     rightAnkleMeasurement = measurement
                     break
             if rightAnkleMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_HIP
             rightHipMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_HIP:
                     rightHipMeasurement = measurement
                     break
             if rightHipMeasurement is None:
-                return 0
+                return None
 
             # Raise error if one of the measurements is missing
             if rightKneeMeasurement is None or rightHipMeasurement is None or rightAnkleMeasurement is None:
@@ -101,43 +107,47 @@ class CalculatedAngles:
                 (rightAnkleMeasurement.x, rightAnkleMeasurement.y),
             )
             return round(rightKneeAngle, 2)
+
         except AttributeError:
-            return 0
+            return None
 
     def calculateRightShoulderAngle(self):
         try:
             # Find the RIGHT_SHOULDER
             rightShoulderMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_SHOULDER:
                     rightShoulderMeasurement = measurement
                     break
             if rightShoulderMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_ELBOW
             rightElbowMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_ELBOW:
                     rightElbowMeasurement = measurement
                     break
             if rightElbowMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_HIP
             rightHipMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_HIP:
                     rightHipMeasurement = measurement
                     break
             if rightHipMeasurement is None:
-                return 0
+                return None
 
             # Raise error if one of the measurements is missing
             if rightShoulderMeasurement is None or rightElbowMeasurement is None or rightHipMeasurement is None:
@@ -150,42 +160,45 @@ class CalculatedAngles:
             )
             return round(shoulderAngle, 2)
         except AttributeError:
-            return 0
+            return None
 
     def calculateRightElbowAngle(self):
         try:
             # Find the RIGHT_SHOULDER
             rightShoulderMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_SHOULDER:
                     rightShoulderMeasurement = measurement
                     break
             if rightShoulderMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_ELBOW
             rightElbowMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_ELBOW:
                     rightElbowMeasurement = measurement
                     break
             if rightElbowMeasurement is None:
-                return 0
+                return None
 
             # Find the RIGHT_WRIST
             rightWristMeasurement = None
             if not self.frameMeasurement.measurements:
-                return 0
+                print("Empty data")
+                return None
             for measurement in self.frameMeasurement.measurements:
                 if measurement.landmark == LandmarkPosition.RIGHT_WRIST:
                     rightWristMeasurement = measurement
                     break
             if rightWristMeasurement is None:
-                return 0
+                return None
 
             # Raise error if one of the measurements is missing
             if rightShoulderMeasurement is None or rightElbowMeasurement is None or rightWristMeasurement is None:
@@ -198,8 +211,54 @@ class CalculatedAngles:
             )
             return round(elbowAngle, 2)
         except AttributeError:
-            return 0
+            return None
+    def calculateLeftKneeAngle(self):
+        try:
+            # Find the LEFT_KNEE
+            leftKneeMeasurement = None
+            if not self.frameMeasurement.measurements:
+                return None
+            for measurement in self.frameMeasurement.measurements:
+                if measurement.landmark == LandmarkPosition.LEFT_KNEE:
+                    leftKneeMeasurement = measurement
+                    break
+            if leftKneeMeasurement is None:
+                return None
 
+            # Find the LEFT_ANKLE
+            leftAnkleMeasurement = None
+            if not self.frameMeasurement.measurements:
+                return None
+            for measurement in self.frameMeasurement.measurements:
+                if measurement.landmark == LandmarkPosition.LEFT_ANKLE:
+                    leftAnkleMeasurement = measurement
+                    break
+            if leftAnkleMeasurement is None:
+                return None
+
+            # Find the LEFT_HIP
+            leftHipMeasurement = None
+            if not self.frameMeasurement.measurements:
+                return None
+            for measurement in self.frameMeasurement.measurements:
+                if measurement.landmark == LandmarkPosition.LEFT_HIP:
+                    leftHipMeasurement = measurement
+                    break
+            if leftHipMeasurement is None:
+                return None
+
+            # Raise error if one of the measurements is missing
+            if leftKneeMeasurement is None or leftHipMeasurement is None or leftAnkleMeasurement is None:
+                raise EmptyDataException("Not enough data to calculate knee angle")
+
+            rightKneeAngle = self.operation.calculateAngle(
+                (leftHipMeasurement.x, leftHipMeasurement.y),
+                (leftKneeMeasurement.x, leftKneeMeasurement.y),
+                (leftAnkleMeasurement.x, leftAnkleMeasurement.y),
+            )
+            return round(rightKneeAngle, 2)
+        except AttributeError:
+            return None
 
 
 
