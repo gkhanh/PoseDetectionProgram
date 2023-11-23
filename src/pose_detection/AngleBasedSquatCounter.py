324292d17db3542ad9ebb47993706d7261a63e09
gkhanh <gkhanhle52@gmail.com>
2023-11-23 14:55:41 +0100

    code refactoring + fully implemented proper squat detector conditions

--- src/pose_detection/AngleBasedSquatCounter.py
+++ src/pose_detection/AngleBasedSquatCounter.py
@@ -1,4 +1,6 @@
 from src.utils.CalculatedAngles import CalculatedAngles
+from src.models.FrameMeasurement import FrameMeasurement
+from src.models.measurement import Measurement, LandmarkPosition
 
 
 # New strategy that uses only angles
@@ -7,7 +9,10 @@ class AngleBasedSquatCounter:
         self.squatAngles = []
         self.stage = None
         self.counter = 0
+        self.lastRightHipAngles = []
         self.lastRightKneeAngles = []
+        self.lastLeftHipAngles = []
+        self.lastLeftKneeAngles = []
 
     def offerMeasurement(self, frameMeasurement):
         # Filter only RIGHT_KNEE
@@ -25,36 +30,83 @@ class AngleBasedSquatCounter:
 
     def isProperSquat(self, frameMeasurement):
         angleCalculator = CalculatedAngles(frameMeasurement)
+        angles = self.getCalculatedAngles(angleCalculator)
+        if any(angle is None for angle in angles):
+            self.resetAngles()
+            return
+        self.appendAngles(angles)
+        self.checkAngleLists()
+        self.checkSquatStage(frameMeasurement)
+
+    def getCalculatedAngles(self, angleCalculator):
         rightHipAngle = angleCalculator.calculateRightHipAngle()
         rightKneeAngle = angleCalculator.calculateRightKneeAngle()
-        rightShoulderAngle = angleCalculator.calculateRightShoulderAngle()
-        rightElbowAngle = angleCalculator.calculateRightElbowAngle()
+        leftHipAngle = angleCalculator.calculateLeftHipAngle()
+        leftKneeAngle = angleCalculator.calculateLeftKneeAngle()
+        return [rightKneeAngle, rightHipAngle, leftKneeAngle, leftHipAngle]
 
-        if rightKneeAngle is None:
-            print("Knee angle is None")
-            self.lastRightKneeAngles = []
-            return
+    def resetAngles(self):
+        print("Knee angle is None or hip angle is None")
+        self.lastRightKneeAngles = []
+        self.lastRightHipAngles = []
+        self.lastLeftKneeAngles = []
+        self.lastLeftHipAngles = []
 
-        self.lastRightKneeAngles.append(rightKneeAngle)
+    def appendAngles(self, angles):
+        self.lastRightHipAngles.append(angles[1])
+        self.lastRightKneeAngles.append(angles[0])
+        self.lastLeftHipAngles.append(angles[3])
+        self.lastLeftKneeAngles.append(angles[2])
 
-        # Limit the last 5 angles
-        if len(self.lastRightKneeAngles) > 5:
-            self.lastRightKneeAngles.pop(0)
+    def checkAngleLists(self):
+        for angleList in [self.lastRightKneeAngles, self.lastRightHipAngles, self.lastLeftKneeAngles,
+                          self.lastLeftHipAngles]:
+            if len(angleList) > 5:
+                angleList.pop(0)
+            if len(angleList) < 5:
+                return
 
-        # We want at least 5 measurements
-        if len(self.lastRightKneeAngles) < 5:
-            return
+    def checkSquatStage(self, frameMeasurement):
+        # For right side
+        self.checkSideSquatStage(self.lastRightKneeAngles, self.lastRightHipAngles, frameMeasurement)
+        # For left side
+        self.checkSideSquatStage(self.lastLeftKneeAngles, self.lastLeftHipAngles, frameMeasurement)
 
-        if all(angle <= 90 for angle in self.lastRightKneeAngles):
-            # We're now in a squat position
-            if self.stage != "down":
+    def checkSideSquatStage(self, kneeAngles, hipAngles, frameMeasurement):
+        if all(angle <= 70 for angle in kneeAngles) and all(angle <= 70 for angle in hipAngles):
+            if self.stage != "down" and self.compareXCoordinateKneeAndFoot(frameMeasurement):
                 print(f'squat count: {self.counter}; stage: down;')
                 self.stage = "down"
                 self.counter += 1
 
-        if all(angle >= 150 for angle in self.lastRightKneeAngles):
-            # We're now in a standing position
-            if self.stage != "up":
-                print('stage: up;')
-                self.stage = "up"
+        if all(angle >= 150 for angle in kneeAngles) and all(angle >= 150 for angle in hipAngles) and self.stage != "up":
+            print('stage: up;')
+            self.stage = "up"
 
+    def compareXCoordinateKneeAndFoot(self, frameMeasurement):
+        rightKneeXCoordinate = None
+        rightFootXCoordinate = None
+        leftKneeXCoordinate = None
+        leftFootXCoordinate = None
+        for measurement in frameMeasurement.measurements:
+            if measurement.landmark == LandmarkPosition.RIGHT_KNEE:
+                rightKneeXCoordinate = measurement.x
+                break
+            if measurement.landmark == LandmarkPosition.RIGHT_FOOT_INDEX:
+                rightFootXCoordinate = measurement.x
+                break
+            if measurement.landmark == LandmarkPosition.LEFT_KNEE:
+                leftKneeXCoordinate = measurement.x
+                break
+            if measurement.landmark == LandmarkPosition.LEFT_FOOT_INDEX:
+                leftFootXCoordinate = measurement.x
+                break
+        print(f"left foot: {leftFootXCoordinate}")
+        print(f"left knee: {leftKneeXCoordinate}")
+        if rightFootXCoordinate is not None and rightKneeXCoordinate is not None and rightKneeXCoordinate - 0.05 < rightFootXCoordinate < rightKneeXCoordinate + 0.05 \
+                or leftFootXCoordinate is not None and leftKneeXCoordinate is not None and leftKneeXCoordinate - 0.05 < leftFootXCoordinate < leftKneeXCoordinate + 0.05:
+            print("Squat detected")
+            return True
+        else:
+            print("Knee or foot not detected")
+            return False
