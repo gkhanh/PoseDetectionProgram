2a26eb547bd6d3b96ea16995f4d4ae48867b431a
gkhanh <gkhanhle52@gmail.com>
2023-12-08 16:47:31 +0100

    finishing up the code to correct the posture

--- src/Rowing_pose_detection/DriveTechniqueAnalyzer.py
+++ src/Rowing_pose_detection/DriveTechniqueAnalyzer.py
@@ -1,9 +1,8 @@
 from src.models.Phase import Phase
-from src.pose_detection.PoseDetector import PoseDetector
 from src.pose_detection.RowingPoseDetector import RowingPoseDetector
-from src.utils.CalculatedAngles import CalculatedAngles
+from src.utils.CalculateAnglesWithNormalizedData import CalculateAnglesWithNormalizedData
 from src.utils.Cancellable import Cancellable
-from src.models.Measurement import LandmarkPosition
+from src.models.NormalizedMeasurement import NormalizedLandmarkPosition
 from src.Rowing_pose_detection.PhaseDetector import PhaseDetector
 
 
@@ -17,6 +16,8 @@ class DriveTechniqueAnalyzer(PhaseDetector.Listener, RowingPoseDetector.Listener
         self.phaseDetectorCancellable = None
         self.listeners = []
 
+        self.feedbackMessage = None
+
         self.currentPhase = phaseDetector.currentPhase
 
     def onPhaseChange(self, currentPhase, frameMeasurementBuffer):
@@ -27,52 +28,96 @@ class DriveTechniqueAnalyzer(PhaseDetector.Listener, RowingPoseDetector.Listener
             if self.poseDetectorCancellable is not None:
                 self.poseDetectorCancellable.cancel()
 
-    def onMeasurement(self, frameMeasurement):
-        self.frameMeasurementBuffer.append(frameMeasurement)
+    def onMeasurement(self, normalizedFrameMeasurement):
+        self.frameMeasurementBuffer.append(normalizedFrameMeasurement)
         self.drivePhaseTechniqueAnalyzer()
 
+    def collectingData(self):
+        firstFrameMeasurement = self.frameMeasurementBuffer[-5]
+        lastFrameMeasurement = self.frameMeasurementBuffer[-1]
+
+        hipAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateHipAngle()
+        hipAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateHipAngle()
+        elbowAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateElbowAngle()
+        elbowAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateElbowAngle()
+        kneeAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateKneeAngle()
+        kneeAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateKneeAngle()
+        shoulderAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateShoulderAngle()
+        shoulderAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateShoulderAngle()
+
+        wristXCoordinate1 = None
+        wristXCoordinate2 = None
+        kneeXCoordinate1 = None
+        kneeXCoordinate2 = None
+
+        for normalizedMeasurement in firstFrameMeasurement.normalizedMeasurements:
+            if normalizedMeasurement.landmark == NormalizedLandmarkPosition.WRIST:
+                wristXCoordinate1 = normalizedMeasurement.x
+            elif normalizedMeasurement.landmark == NormalizedLandmarkPosition.KNEE:
+                kneeXCoordinate1 = normalizedMeasurement.x
+        for normalizedMeasurement in lastFrameMeasurement.normalizedMeasurements:
+            if normalizedMeasurement.landmark == NormalizedLandmarkPosition.WRIST:
+                wristXCoordinate2 = normalizedMeasurement.x
+            elif normalizedMeasurement.landmark == NormalizedLandmarkPosition.KNEE:
+                kneeXCoordinate2 = normalizedMeasurement.x
+
+        dataCollection = [hipAngle1, hipAngle2, elbowAngle1, elbowAngle2, kneeAngle1, kneeAngle2, shoulderAngle1,
+                          shoulderAngle2, wristXCoordinate1, wristXCoordinate2, kneeXCoordinate1, kneeXCoordinate2]
+        return dataCollection
+
     def drivePhaseTechniqueAnalyzer(self):
-        for frameMeasurement in self.frameMeasurementBuffer:
-            leftHipAngle = CalculatedAngles(frameMeasurement).calculateLeftHipAngle()
-            rightHipAngle = CalculatedAngles(frameMeasurement).calculateRightHipAngle()
-            leftElbowAngle = CalculatedAngles(frameMeasurement).calculateLeftElbowAngle()
-            rightElbowAngle = CalculatedAngles(frameMeasurement).calculateRightElbowAngle()
-            leftKneeAngle = CalculatedAngles(frameMeasurement).calculateLeftKneeAngle()
-            rightKneeAngle = CalculatedAngles(frameMeasurement).calculateRightKneeAngle()
-            leftShoulderAngle = CalculatedAngles(frameMeasurement).calculateLeftShoulderAngle()
-            rightShoulderAngle = CalculatedAngles(frameMeasurement).calculateRightShoulderAngle()
+        dataCollection = self.collectingData()
+        previousHipAngle = dataCollection[0]
+        currentHipAngle = dataCollection[1]
+        previousElbowAngle = dataCollection[2]
+        currentElbowAngle = dataCollection[3]
+        previousKneeAngle = dataCollection[4]
+        currentKneeAngle = dataCollection[5]
+        previousShoulderAngle = dataCollection[6]
+        currentShoulderAngle = dataCollection[7]
+        previousWristXCoordinate = dataCollection[8]
+        currentWristXCoordinate = dataCollection[9]
+        previousKneeXCoordinate = dataCollection[10]
+        currentKneeXCoordinate = dataCollection[11]
+        if dataCollection is not None:
+            # measure the knee angles, check if the knees angles is less than 100 degree, then the hip angles has to be stayed less than 90 degree
+            # else give feedback on hip: open hip too soon
+            if currentKneeAngle < 100:
+                if currentHipAngle >= 90:
+                    print("Feedback: Open hip too soon")
+                    self.feedbackMessage = "Open hip too soon"
 
-            leftWristXCoordinate = None
-            rightWristXCoordinate = None
-            leftKneeCoordinate = None
-            rightKneeCoordinate = None
-            for measurement in frameMeasurement.measurements:
-                if measurement.landmark == LandmarkPosition.LEFT_WRIST:
-                    leftWristXCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.RIGHT_WRIST:
-                    rightWristXCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.LEFT_KNEE:
-                    leftKneeCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.RIGHT_KNEE:
-                    rightKneeCoordinate = measurement.x
+            # measure the hip angles, check if the knees angles is more than 100 degree, then check if the hip angles is increasing or not
+            # else give feedback on hip opening: not opening hip
+            elif previousKneeAngle > 100:
+                if previousHipAngle <= 90 and previousHipAngle - 5 < currentHipAngle < previousHipAngle + 5:
+                    print("Feedback: Hip is not open")
+                    self.feedbackMessage = "Hip is not open"
 
-        # TODO: measure the knee angles, check if the knees angles is less than 100 degree, then the hip angles has to be stay less than 90 degree
-        # else give feedback on hip: open hip too soon
+            # check if the knee angles is around 180 degree (fully extended) and hip angles is more than 100 degree
+            # else give feedback on hip opening: not opening hip, leg not fully extended
+            if currentKneeAngle < 140:
+                print("Feedback: Leg not fully extended")
+                self.feedbackMessage = "Leg not fully extended"
+            else:
+                if currentHipAngle < 100 and previousHipAngle - 5 < currentHipAngle < previousHipAngle + 5:
+                    print("Feedback: Hip is not open")
+                    self.feedbackMessage = "Hip is not open"
 
-        # TODO: measure the hip angles, check if the knees angles is more than 100 degree, then check if the hip angles is increasing or not
-        # else give feedback on hip opening: not opening hip
+            # check when the user can pull back the arm by comparing the x coordinates of both wrists and knees, if they are equal
+            # else give feedback: pull arm too soon/early
+            # check if the previous condition is met, then check the x coordinates of both wrists if its decreasing(?) and shoulder angle is decreasing
+            # else give feedback: arm not pulled back
+            if currentKneeXCoordinate - 0.05 <= currentWristXCoordinate <= currentKneeXCoordinate + 0.05:
+                if not previousWristXCoordinate < currentWristXCoordinate:
+                    print("Feedback: Not pulling arm")
+                    self.feedbackMessage = "Not pulling arm"
 
-        # TODO: check if the knee angles is around 180 degree (fully extended) and hip angles is more than 100 degree
-        # else give feedback on hip opening: not opening hip, leg not fully extended
-
-        # TODO: check when the user can pull back the arm by comparing the x coordinates of both wrists and knees, if they are equal
-        # else give feedback: pull arm too soon/early
-
-        # TODO: check if the previous condition is met, then check the x coordinates of both wrists if its decreasing(?) and shoulder angle is decreasing
-        # else give feedback: arm not pulled back
-
-        # TODO: check if the arms are fully pulled back by checking the shoulder angla is less than 10 degree and elbow angles is around 90 degree
-        # else give feedback: arm not pulled back properly
+            # check if the arms are fully pulled back by checking the shoulder angla is less than 10 degree and elbow angles is around 90 degree
+            # else give feedback: arm not pulled back properly
+            if not currentShoulderAngle < 10 and 60 < currentElbowAngle <= 95:
+                print("Feedback: Arm not pulled back properly")
+                self.feedbackMessage = "Arm not pulled back properly"
 
     def addListener(self, listener):
         self.listeners.append(listener)
@@ -91,5 +136,5 @@ class DriveTechniqueAnalyzer(PhaseDetector.Listener, RowingPoseDetector.Listener
 
     class Listener:
 
-        def driveTechniqueAnalyzer(self):
+        def driveTechniqueAnalyzer(self, feedbackMessage):
             raise NotImplementedError
--- src/Rowing_pose_detection/RecoveryTechniqueAnalyzer.py
+++ src/Rowing_pose_detection/RecoveryTechniqueAnalyzer.py
@@ -16,6 +16,8 @@ class RecoveryTechniqueAnalyzer(PhaseDetector.Listener, PoseDetector.Listener):
         self.phaseDetectorCancellable = None
         self.listeners = []
 
+        self.feedbackMessage = None
+
         self.currentPhase = phaseDetector.currentPhase
 
     def onPhaseChange(self, currentPhase, frameMeasurementBuffer):
@@ -28,39 +30,79 @@ class RecoveryTechniqueAnalyzer(PhaseDetector.Listener, PoseDetector.Listener):
 
     def onMeasurement(self, frameMeasurement):
         self.frameMeasurementBuffer.append(frameMeasurement)
+        self.recoveryPhaseTechniqueAnalyzer()
 
-    def recoveryPhaseTechniqueAnalyzer(self, frameMeasurementBuffer):
-        for frameMeasurement in frameMeasurementBuffer:
-            leftHipAngle = CalculatedAngles(frameMeasurement).calculateLeftHipAngle()
-            rightHipAngle = CalculatedAngles(frameMeasurement).calculateRightHipAngle()
-            leftElbowAngle = CalculatedAngles(frameMeasurement).calculateLeftElbowAngle()
-            rightElbowAngle = CalculatedAngles(frameMeasurement).calculateRightElbowAngle()
-            leftKneeAngle = CalculatedAngles(frameMeasurement).calculateLeftKneeAngle()
-            rightKneeAngle = CalculatedAngles(frameMeasurement).calculateRightKneeAngle()
-            leftShoulderAngle = CalculatedAngles(frameMeasurement).calculateLeftShoulderAngle()
-            rightShoulderAngle = CalculatedAngles(frameMeasurement).calculateRightShoulderAngle()
+    def collectingData(self):
+        firstFrameMeasurement = self.frameMeasurementBuffer[-5]
+        lastFrameMeasurement = self.frameMeasurementBuffer[-1]
 
-            leftWristXCoordinate = None
-            rightWristXCoordinate = None
-            for measurement in frameMeasurement.measurements:
-                if measurement.landmark == LandmarkPosition.LEFT_WRIST:
-                    leftWristXCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.RIGHT_WRIST:
-                    rightWristXCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.LEFT_KNEE:
-                    leftKneeCoordinate = measurement.x
-                elif measurement.landmark == LandmarkPosition.RIGHT_KNEE:
-                    rightKneeCoordinate = measurement.x
+        hipAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateHipAngle()
+        hipAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateHipAngle()
+        elbowAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateElbowAngle()
+        elbowAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateElbowAngle()
+        kneeAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateKneeAngle()
+        kneeAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateKneeAngle()
+        shoulderAngle1 = CalculateAnglesWithNormalizedData(firstFrameMeasurement).calculateShoulderAngle()
+        shoulderAngle2 = CalculateAnglesWithNormalizedData(lastFrameMeasurement).calculateShoulderAngle()
 
-            # TODO: check if the arms are straightening first by measuring the elbow angles and the shoulder angle is increasing
+        wristXCoordinate1 = None
+        wristXCoordinate2 = None
+        kneeXCoordinate1 = None
+        kneeXCoordinate2 = None
+
+        for normalizedMeasurement in firstFrameMeasurement.normalizedMeasurements:
+            if normalizedMeasurement.landmark == NormalizedLandmarkPosition.WRIST:
+                wristXCoordinate1 = normalizedMeasurement.x
+            elif normalizedMeasurement.landmark == NormalizedLandmarkPosition.KNEE:
+                kneeXCoordinate1 = normalizedMeasurement.x
+        for normalizedMeasurement in lastFrameMeasurement.normalizedMeasurements:
+            if normalizedMeasurement.landmark == NormalizedLandmarkPosition.WRIST:
+                wristXCoordinate2 = normalizedMeasurement.x
+            elif normalizedMeasurement.landmark == NormalizedLandmarkPosition.KNEE:
+                kneeXCoordinate2 = normalizedMeasurement.x
+
+        dataCollection = [hipAngle1, hipAngle2, elbowAngle1, elbowAngle2, kneeAngle1, kneeAngle2, shoulderAngle1,
+                          shoulderAngle2, wristXCoordinate1, wristXCoordinate2, kneeXCoordinate1, kneeXCoordinate2]
+        return dataCollection
+
+    def recoveryPhaseTechniqueAnalyzer(self):
+        dataCollection = self.collectingData()
+        previousHipAngle = dataCollection[0]
+        currentHipAngle = dataCollection[1]
+        previousElbowAngle = dataCollection[2]
+        currentElbowAngle = dataCollection[3]
+        previousKneeAngle = dataCollection[4]
+        currentKneeAngle = dataCollection[5]
+        previousShoulderAngle = dataCollection[6]
+        currentShoulderAngle = dataCollection[7]
+        previousWristXCoordinate = dataCollection[8]
+        currentWristXCoordinate = dataCollection[9]
+        previousKneeXCoordinate = dataCollection[10]
+        currentKneeXCoordinate = dataCollection[11]
+
+        if dataCollection is not None:
+            pass
+            # check if the arms are straightening first by measuring the elbow angles and the shoulder angle is increasing
             # else give feedback on arms: not straightening arms
+            if 150 < currentElbowAngle < 180:
+                if not previousShoulderAngle < currentShoulderAngle:
+                    print("Feedback: Arms are not straightening")
+                    self.feedbackMessage = "Arms are not straightening"
 
-            # TODO: make sure that the arms must moving first then the body will pivot after and before the leg bends
+            # make sure that the arms must move first then the body will pivot after and before the leg bends
             # by making if the arm move then check if the hip angle is decreasing or not and check if the leg angle is still around 180
             # else give feedback: need to straighten the arms first / close the body / keep the leg straight
+            if previousWristXCoordinate < currentWristXCoordinate or previousElbowAngle > currentElbowAngle:
+                if not previousHipAngle < currentHipAngle:
+                    print("Feedback: Need to open hip")
+                    self.feedbackMessage = "Need to open hip"
 
-            # TODO: after 2 conditions above are met then check if the knee angles are decreasing and make sure the final knee angles is around 30
+            # after 2 conditions above are met then check if the knee angles are decreasing and make sure the final knee angles is around 30
             # else give feedback: leg is not bent properly
+            if previousKneeXCoordinate > currentKneeXCoordinate or previousKneeAngle < currentKneeAngle:
+                if not 35 > previousKneeAngle > 10:
+                    print("Feedback: Leg is not bent properly")
+                    self.feedbackMessage = "Leg is not bent properly"
 
     def addListener(self, listener):
         self.listeners.append(listener)
@@ -79,5 +121,5 @@ class RecoveryTechniqueAnalyzer(PhaseDetector.Listener, PoseDetector.Listener):
 
     class Listener:
 
-        def recoveryTechniqueAnalyzer(self):
+        def recoveryTechniqueAnalyzer(self, feedbackMessage):
             raise NotImplementedError
